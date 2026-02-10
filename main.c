a178868e03d8b486ac31c01d0c1c5311f14fcf4a
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-02-10 07:16:16 +0100

    Add initial game logic

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include "board.h"
+#include "snake.h"
 
 int main(int argc, char **argv) {
-	printf("Hello, World!\n");
+	Board_t board;
+	Snake_t snake;
+	init_board(&board, 20, 50);	
+	init_snake(&snake, &board);
+	draw_board(&board);
+	getchar();
+	bool game_over = false;
+	while (!game_over) {
+		move_snake(&snake, &board, &game_over);
+		getchar();
+	}
+	free_board(&board);
+	free_snake(&snake);
 	return 0;
 }
