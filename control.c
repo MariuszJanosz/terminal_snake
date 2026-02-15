9f35ba204c0b976c855d5387cfdc9d611155d684
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-02-15 19:52:45 +0100

    Factor out setting snake direction to a separate function

--- control.c
+++ control.c
@@ -10,6 +10,22 @@
 
 Direction_t new_direction = RIGHT;
 
+void set_snake_direction(mtx_t *mtx, cnd_t *cnd, Direction_t direction) {
+	if (mtx_lock(mtx) == thrd_error) {
+		assert(false && "mtx_lock failed!");
+		exit(1);
+	}
+	new_direction = direction;
+	if (mtx_unlock(mtx) == thrd_error) {
+		assert(false && "mtx_unlock failed!");
+		exit(1);
+	}
+	if (cnd_signal(cnd) == thrd_error) {
+		assert(false && "cnd_signal failed!");
+		exit(1);
+	}
+}
+
 int control(void *context) {
 	Control_context_t control_context = *(Control_context_t*)context;
 	Snake_t *snake = control_context.snake;
@@ -27,70 +43,22 @@ int control(void *context) {
 			case 'W':
 			case 'w':
 				if (snake->direction == DOWN) break;
-				if (mtx_lock(mtx) == thrd_error) {
-					assert(false && "mtx_lock failed!");
-					exit(1);
-				}
-				new_direction = UP;
-				if (mtx_unlock(mtx) == thrd_error) {
-					assert(false && "mtx_unlock failed!");
-					exit(1);
-				}
-				if (cnd_signal(cnd) == thrd_error) {
-					assert(false && "cnd_signal failed!");
-					exit(1);
-				}
+				set_snake_direction(mtx, cnd, UP);
 				break;
 			case 'D':
 			case 'd':
 				if (snake->direction == LEFT) break;
-				if (mtx_lock(mtx) == thrd_error) {
-					assert(false && "mtx_lock failed!");
-					exit(1);
-				}
-				new_direction = RIGHT;
-				if (mtx_unlock(mtx) == thrd_error) {
-					assert(false && "mtx_unlock failed!");
-					exit(1);
-				}
-				if (cnd_signal(cnd) == thrd_error) {
-					assert(false && "cnd_signal failed!");
-					exit(1);
-				}
+				set_snake_direction(mtx, cnd, RIGHT);
 				break;
 			case 'S':
 			case 's':
 				if (snake->direction == UP) break;
-				if (mtx_lock(mtx) == thrd_error) {
-					assert(false && "mtx_lock failed!");
-					exit(1);
-				}
-				new_direction = DOWN;
-				if (mtx_unlock(mtx) == thrd_error) {
-					assert(false && "mtx_unlock failed!");
-					exit(1);
-				}
-				if (cnd_signal(cnd) == thrd_error) {
-					assert(false && "cnd_signal failed!");
-					exit(1);
-				}
+				set_snake_direction(mtx, cnd, DOWN);
 				break;
 			case 'A':
 			case 'a':
 				if (snake->direction == RIGHT) break;
-				if (mtx_lock(mtx) == thrd_error) {
-					assert(false && "mtx_lock failed!");
-					exit(1);
-				}
-				new_direction = LEFT;
-				if (mtx_unlock(mtx) == thrd_error) {
-					assert(false && "mtx_unlock failed!");
-					exit(1);
-				}
-				if (cnd_signal(cnd) == thrd_error) {
-					assert(false && "cnd_signal failed!");
-					exit(1);
-				}
+				set_snake_direction(mtx, cnd, LEFT);
 				break;
 			case EOF:
 				if (!atomic_load(game_over)) break;
